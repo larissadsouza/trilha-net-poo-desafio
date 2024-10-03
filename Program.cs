b5edf312579b9c105861415007290c31d343f9d9
larissadsouza <larissasouza096@gmail.com>
2024-10-03 19:36:23 -0300

    Implementação de classes e métodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celNokia = new Nokia("11 95454-5454", "Moto G", "78789879879", 256);
+celNokia.Ligar();
+celNokia.ReceberLigacao();
+celNokia.InstalarAplicativo("WhatsApp");
+
+Iphone celIphone = new Iphone("11 95454-5454", "Iphone 16", "78789879879", 256);
+celIphone.Ligar();
+celIphone.ReceberLigacao();
+celIphone.InstalarAplicativo("WhatsApp");
